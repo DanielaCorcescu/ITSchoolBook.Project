510c3ff21012a6388b4b6e437f533de681b35f39
Corcescu <corcescudaniela@yahoo.com>
2021-04-16 22:05:19 +0200

    Implemented add book feature

--- ItSchoolBookProject.py
+++ ItSchoolBookProject.py
@@ -1,28 +1,40 @@
-def add_book():
-    print("Add a book option")
-def list_book():
+def AddBook():
+    book_name = input("Insert a book name ->")
+    author_name = input("Insert a author name ->")
+    #impor int csv lib
+    import csv
+    with open('bookDB.csv','w') as file:
+        writer = csv.DictWriter(file,fieldnames=[
+            "BookName", "AuthorName", "ShareWith", "IsRead"
+        ])
+        writer.writerow({"BookName": book_name,
+                         "AuthorName": author_name})
+    print("Book was added successfully")
+
+
+def ListBook():
     print("List books option")
-def update_book():
+def UpdateBook():
     print("Update a book option")
-def share_book():
+def ShareBook():
     print("Share a book option")
 
-
 # Maine menu for user
-print("Menu : ")
-print("1 : Add a book")
-print("2 : List books")
+print("Meniu : ")
+print("1 : Add a book" )
+print("2 : List books" )
 print("3 : Update book")
 print("4 : Share book")
-option = int(input("Select oane option -> "))
+option = int(input("Select one option -> "))
+
 
 if option == 1:
-    add_book()
+    AddBook()
 elif option == 2:
-    list_book()
+    ListBook()
 elif option == 3:
-    update_book()
+    UpdateBook()
 elif option == 4:
-    share_book()
+    ShareBook()
 else:
     print("Not a valid option")
